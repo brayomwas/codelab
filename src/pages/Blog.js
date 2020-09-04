800a16252086548c713068775bba55303b62add1
Cabral Chege <cabralowiro@gmail.com>
2020-09-04 10:50:35 +0300

    added the explore blogs

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -38,7 +38,13 @@ const useStyles = makeStyles((theme) => ({
     display:"flex",
     flexDirection:"column"
   },
+  mediaImg:{
+    height:300,
+    width:350
+  },
   contentFeature:{
+    alignContent:"right",
+    alignItems:"right",
     display:"flex",
     flexDirection:"column",
     justifyContent:"right"
@@ -57,7 +63,15 @@ const useStyles = makeStyles((theme) => ({
   },
   author:{
     display:"flex"
-  }
+  },
+  mediaFeatureOther:{ maxWidth:"50%",
+  display:"flex",
+  flexDirection:"column"
+},
+mediaOther:{
+  height:150,
+  width:150
+}
 }));
 
 function Blog() {
@@ -86,16 +100,17 @@ function Blog() {
           <Grid container spacing={3}>
             <Grid item xs={12}>
               <Card className={classes.cardFeature}>
-                  <CardActionArea>
+                  
                     <Box className={classes.mediaFeature}>
-                      <CardMedia className={classes.media}
+                      <CardMedia className={classes.mediaImg}
                         component="img"
                         alt="Contemplative Reptile"
                         height="140"                        
-                        image="https://images.unsplash.com/photo-1589988047635-39bf08b1720c?ixl
-                        ib=rb-1.2.1&auto=format&fit=crop&w=1423&q=80"
+                        image="https://images.unsplash.com/photo-1594299751283-c2f793842a52?ixlib=rb-1.2.1&ix
+                        eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80"
                       />
                     </Box>
+                    <CardActionArea>
                         <CardContent>
                           <Box className={classes.contentFeature}>
                             <Typography gutterBottom variant="h5" component="h2">
@@ -107,7 +122,7 @@ function Blog() {
                             </Typography>
                             </Box>
                         </CardContent>
-                      </CardActionArea>
+                  
                       <CardActions className={classes.cardActions}>
                         <Box className={classes.author}>
                           <Avatar 
@@ -127,6 +142,7 @@ function Blog() {
                           </Box>
                         </Box>
                     </CardActions>
+                    </CardActionArea>
                 </Card>
             </Grid>
 
@@ -150,7 +166,7 @@ function Blog() {
                       height="140"
                       image="https://images.unsplash.com/photo-1589988047635-39bf08b1720c?ixl
                       ib=rb-1.2.1&auto=format&fit=crop&w=1423&q=80"
-                      title="Contemplative Reptile"
+                      
                     />
                     <CardContent>
                       <Typography gutterBottom variant="h5" component="h2">
@@ -192,7 +208,7 @@ function Blog() {
                       alt="Contemplative Reptile"
                       height="140"
                       image="https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80"
-                      title="Contemplative Reptile"
+                      
                     />
                     <CardContent>
                       <Typography gutterBottom variant="h5" component="h2">
@@ -243,9 +259,9 @@ function Blog() {
                     component="img"
                     alt="Contemplative Reptile"
                     height="140"
-                    image="https://images.unsplash.com/photo-1589988047635-39bf08b1720c?ixl
-                    ib=rb-1.2.1&auto=format&fit=crop&w=1423&q=80"
-                    title="Contemplative Reptile"
+                    image="https://images.unsplash.com/photo-1581093448798-fa7eab922c96?ixlib=rb-1.2.1&ix
+                    id=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+                    
                   />
                   <CardContent>
                     <Typography gutterBottom variant="h5" component="h2">
@@ -285,9 +301,9 @@ function Blog() {
                     component="img"
                     alt="Contemplative Reptile"
                     height="140"
-                    image="https://images.unsplash.com/photo-1589988047635-39bf08b1720c?ixl
-                    ib=rb-1.2.1&auto=format&fit=crop&w=1423&q=80"
-                    title="Contemplative Reptile"
+                    image="https://images.unsplash.com/photo-1593694605442-1168e359fc2e?ixlib=rb-1.2.1&ixid=
+                    eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+                    
                   />
                   <CardContent>
                     <Typography gutterBottom variant="h5" component="h2">
@@ -327,9 +343,9 @@ function Blog() {
                     component="img"
                     alt="Contemplative Reptile"
                     height="140"
-                    image="https://images.unsplash.com/photo-1589988047635-39bf08b1720c?ixl
-                    ib=rb-1.2.1&auto=format&fit=crop&w=1423&q=80"
-                    title="Contemplative Reptile"
+                    image="https://images.unsplash.com/photo-1471107340929-a87cd0f5b5f3?ixlib=rb-1.2.1&ix
+                    d=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=666&q=80"
+                    
                   />
                   <CardContent>
                     <Typography gutterBottom variant="h5" component="h2">
@@ -367,6 +383,254 @@ function Blog() {
           <Typography variant="h6" className={classes.blogTitle}>
             Explore Other Blogs
           </Typography>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+            <Card className={classes.cardFeature}>
+                  
+                  <Box className={classes.mediaFeatureOther }>
+                    <CardMedia className={classes.mediaOther}
+                      component="img"
+                      alt="Contemplative Reptile"
+                      height="140"                        
+                      image="https://images.unsplash.com/photo-1589988047635-39bf08b1720c?ixl
+                      ib=rb-1.2.1&auto=format&fit=crop&w=1423&q=80"
+                    />
+                  </Box>
+                  <CardActionArea>
+                      <CardContent>
+                        <Box className={classes.contentFeature}>
+                          <Typography gutterBottom variant="h5" component="h2">
+                            Top 10 Javascript Libraries
+                          </Typography>
+                          <Typography variant="body2" color="textSecondary" component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarctica
+                          </Typography>
+                          </Box>
+                      </CardContent>
+                
+                    <CardActions className={classes.cardActions}>
+                      <Box className={classes.author}>
+                        <Avatar 
+                        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?
+                        ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+                        />
+                        <Box ml={2}>
+                          <Typography variant="subtitle2" component="p">
+                          Cabral Chege
+                          </Typography>
+                          <Typography variant="subtitle2" color="textSecondary" component="p">
+                          January 16,2020
+                          </Typography>
+                        </Box>
+                        <Box>
+                          <BookmarkBorderIcon />
+                        </Box>
+                      </Box>
+                  </CardActions>
+                  </CardActionArea>
+              </Card>
+
+
+            </Grid>
+
+            <Grid item xs={12}>
+            <Card className={classes.cardFeature}>
+                  
+                  <Box className={classes.mediaFeatureOther }>
+                    <CardMedia className={classes.mediaOther}
+                      component="img"
+                      alt="Contemplative Reptile"
+                      height="140"                        
+                      image="https://images.unsplash.com/photo-1589988047635-39bf08b1720c?ixl
+                      ib=rb-1.2.1&auto=format&fit=crop&w=1423&q=80"
+                    />
+                  </Box>
+                  <CardActionArea>
+                      <CardContent>
+                        <Box className={classes.contentFeature}>
+                          <Typography gutterBottom variant="h5" component="h2">
+                            Top 10 Javascript Libraries
+                          </Typography>
+                          <Typography variant="body2" color="textSecondary" component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarctica
+                          </Typography>
+                          </Box>
+                      </CardContent>
+                
+                    <CardActions className={classes.cardActions}>
+                      <Box className={classes.author}>
+                        <Avatar 
+                        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?
+                        ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+                        />
+                        <Box ml={2}>
+                          <Typography variant="subtitle2" component="p">
+                          Cabral Chege
+                          </Typography>
+                          <Typography variant="subtitle2" color="textSecondary" component="p">
+                          January 16,2020
+                          </Typography>
+                        </Box>
+                        <Box>
+                          <BookmarkBorderIcon />
+                        </Box>
+                      </Box>
+                  </CardActions>
+                  </CardActionArea>
+              </Card>
+
+
+            </Grid>
+            <Grid item xs={12}>
+            <Card className={classes.cardFeature}>
+                  
+                  <Box className={classes.mediaFeatureOther }>
+                    <CardMedia className={classes.mediaOther}
+                      component="img"
+                      alt="Contemplative Reptile"
+                      height="140"                        
+                      image="https://images.unsplash.com/photo-1598467829413-e58f9288dd34?ixlib=rb-1.2.1&ixid=eyJhcHBfa
+                      WQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+                    />
+                  </Box>
+                  <CardActionArea>
+                      <CardContent>
+                        <Box className={classes.contentFeature}>
+                          <Typography gutterBottom variant="h5" component="h2">
+                            Top 10 Javascript Libraries
+                          </Typography>
+                          <Typography variant="body2" color="textSecondary" component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarctica
+                          </Typography>
+                          </Box>
+                      </CardContent>
+                
+                    <CardActions className={classes.cardActions}>
+                      <Box className={classes.author}>
+                        <Avatar 
+                        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?
+                        ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+                        />
+                        <Box ml={2}>
+                          <Typography variant="subtitle2" component="p">
+                          Cabral Chege
+                          </Typography>
+                          <Typography variant="subtitle2" color="textSecondary" component="p">
+                          January 16,2020
+                          </Typography>
+                        </Box>
+                        <Box>
+                          <BookmarkBorderIcon />
+                        </Box>
+                      </Box>
+                  </CardActions>
+                  </CardActionArea>
+              </Card>
+
+
+            </Grid>
+            <Grid item xs={12}>
+            <Card className={classes.cardFeature}>
+                  
+                  <Box className={classes.mediaFeatureOther }>
+                    <CardMedia className={classes.mediaOther}
+                      component="img"
+                      alt="Contemplative Reptile"
+                      height="140"                        
+                      image="https://images.unsplash.com/photo-1589988047635-39bf08b1720c?ixl
+                      ib=rb-1.2.1&auto=format&fit=crop&w=1423&q=80"
+                    />
+                  </Box>
+                  <CardActionArea>
+                      <CardContent>
+                        <Box className={classes.contentFeature}>
+                          <Typography gutterBottom variant="h5" component="h2">
+                            Top 10 Javascript Libraries
+                          </Typography>
+                          <Typography variant="body2" color="textSecondary" component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarctica
+                          </Typography>
+                          </Box>
+                      </CardContent>
+                
+                    <CardActions className={classes.cardActions}>
+                      <Box className={classes.author}>
+                        <Avatar 
+                        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?
+                        ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+                        />
+                        <Box ml={2}>
+                          <Typography variant="subtitle2" component="p">
+                          Cabral Chege
+                          </Typography>
+                          <Typography variant="subtitle2" color="textSecondary" component="p">
+                          January 16,2020
+                          </Typography>
+                        </Box>
+                        <Box>
+                          <BookmarkBorderIcon />
+                        </Box>
+                      </Box>
+                  </CardActions>
+                  </CardActionArea>
+              </Card>
+
+
+            </Grid>
+            <Grid item xs={12}>
+            <Card className={classes.cardFeature}>
+                  
+                  <Box className={classes.mediaFeatureOther }>
+                    <CardMedia className={classes.mediaOther}
+                      component="img"
+                      alt="Contemplative Reptile"
+                      height="140"                        
+                      image="https://images.unsplash.com/photo-1598346676061-628d28d47ddf?ixlib=rb-1.2.1&ixid=eyJhcHBfa
+                      WQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+                    />
+                  </Box>
+                  <CardActionArea>
+                      <CardContent>
+                        <Box className={classes.contentFeature}>
+                          <Typography gutterBottom variant="h5" component="h2">
+                            Top 10 Javascript Libraries
+                          </Typography>
+                          <Typography variant="body2" color="textSecondary" component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarctica
+                          </Typography>
+                          </Box>
+                      </CardContent>
+                
+                    <CardActions className={classes.cardActions}>
+                      <Box className={classes.author}>
+                        <Avatar 
+                        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?
+                        ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+                        />
+                        <Box ml={2}>
+                          <Typography variant="subtitle2" component="p">
+                          Cabral Chege
+                          </Typography>
+                          <Typography variant="subtitle2" color="textSecondary" component="p">
+                          January 16,2020
+                          </Typography>
+                        </Box>
+                        <Box>
+                          <BookmarkBorderIcon />
+                        </Box>
+                      </Box>
+                  </CardActions>
+                  </CardActionArea>
+              </Card>
+
+
+            </Grid>
+            </Grid>
           </Container>
       </Container>
       </Box>
